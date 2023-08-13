cdc2bdcb4d05c289418be045139056b2c1405a3c
Rajesh Narayan <80790985+rnarayan1337@users.noreply.github.com>
2023-08-13 00:43:33 +0000

    adding boiler plate code

---
+++ sample.py
@@ -0,0 +1,10 @@
+def add(x, y):
+    return x + y
+
+def main():
+    print('In Sample')
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ test_sample.py
@@ -0,0 +1,4 @@
+from sample import add
+
+def test_add():
+    assert 2 == add( 1, 1)
